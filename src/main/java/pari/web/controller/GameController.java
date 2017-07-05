808940eb8a3e7907619804f210c135a2bdbd9534
Borys Sheludchenko <Borys_Sheludchenko1@epam.com>
2017-07-05 18:05:55 +0300

    initial

--- src/main/java/pari/web/controller/GameController.java
+++ src/main/java/pari/web/controller/GameController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import pari.business.dao.GameDao;
 import pari.business.model.Game;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/game")
 public class GameController {
--- src/main/java/pari/web/controller/Hello.java
+++ src/main/java/pari/web/controller/Hello.java
@@ -1,9 +1,11 @@
 package pari.web.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class Hello {
 
