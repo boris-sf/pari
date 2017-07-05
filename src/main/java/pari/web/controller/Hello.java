5372446e73e1c9af700724cd9a457418dca9790c
Borys Sheludchenko <Borys_Sheludchenko1@epam.com>
2017-07-05 18:08:02 +0300

    initial

--- src/main/java/pari/web/controller/Hello.java
+++ src/main/java/pari/web/controller/Hello.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
+@CrossOrigin("*")
 public class Hello {
 
 	@GetMapping("/game/{id}")
