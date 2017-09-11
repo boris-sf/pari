55d8b6cffb66d92ed38c70d46991384902d80315
bsheludchenko <bsheludchenko@cogniance.com>
2017-09-11 17:11:11 +0300

    get invitations

--- src/main/java/pari/business/service/InvitationService.java
+++ src/main/java/pari/business/service/InvitationService.java
@@ -1,5 +1,8 @@
 package pari.business.service;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +30,10 @@ public class InvitationService {
 		final User user = users.currentUser();
 		final Invitation invitation = invitations.lookup(user.id(), userId, gameId);
 		return invitation == null ? invitations.save(new Invitation(user, users.find(userId), game)) : invitation;
+	}
+
+	public List<Invitation> active() {
+		final User user = users.currentUser();
+		return invitations.lookup(user.id(), new Date());
 	}
 }
--- src/main/java/pari/web/controller/InvitationController.java
+++ src/main/java/pari/web/controller/InvitationController.java
@@ -1,7 +1,10 @@
 package pari.web.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,10 @@ public class InvitationController {
 	@PostMapping
 	public Invitation create(@RequestBody NewInvitationDto invitation) {
 		return invitations.create(invitation.getUserId(), invitation.getGameId());
+	}
+
+	@GetMapping
+	public List<Invitation> all() {
+		return invitations.active();
 	}
 }
